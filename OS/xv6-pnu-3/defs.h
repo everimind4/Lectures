e5073805fdc02be07f14763426ae940a4a7bd753
everimind4 <everimind4@gmail.com>
2021-05-12 19:25:14 +0900

    Project #3 : Stack Growth

--- OS/xv6-pnu-3/defs.h
+++ OS/xv6-pnu-3/defs.h
@@ -177,6 +177,8 @@ void            kvmalloc(void);
 pde_t*          setupkvm(void);
 char*           uva2ka(pde_t*, char*);
 int             allocuvm(pde_t*, uint, uint);
+int             allocuvm_forstack(pde_t*, uint, uint);
+void            pagefault(void);
 int             deallocuvm(pde_t*, uint, uint);
 void            freevm(pde_t*);
 void            inituvm(pde_t*, char*, uint);
